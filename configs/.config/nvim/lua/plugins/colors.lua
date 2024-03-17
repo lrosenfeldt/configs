edd5805194d74e54c0d5925ebb30d83ddedb14ce
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-03-17 14:18:08 +0100

    catppuccin all the way

--- configs/.config/nvim/lua/plugins/colors.lua
+++ configs/.config/nvim/lua/plugins/colors.lua
@@ -4,10 +4,8 @@ return {
 		name = "catppuccin",
 		priority = 9000,
 		config = function()
+			require("catppuccin").setup({ flavour = "mocha" })
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
-	-- { "NLKNguyen/papercolor-theme", priority = 9000 },
-	-- Theme inspired by Atom
-	-- { "Mofiqul/vscode.nvim", priority = 9000 },
 }
