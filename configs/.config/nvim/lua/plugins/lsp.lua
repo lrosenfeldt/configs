2e44d0d7445e645ec40aff5e2e68eaa16cee1cf5
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-04-18 08:38:30 +0200

    fix(vue-lsp): change settings of volar to be compatible with volar (vue-lsp) v2
    [See this issue on mason-lspconfig](https://github.com/williamboman/mason-lspconfig.nvim/issues/371)
    
    this is propably a workaround and I should check if I can do it with
    typescript plugins

--- configs/.config/nvim/lua/plugins/lsp.lua
+++ configs/.config/nvim/lua/plugins/lsp.lua
@@ -96,6 +96,14 @@ return {
 						"vue",
 						"json",
 					},
+					init_options = {
+						vue = {
+							hybridMode = false,
+						},
+						typescript = {
+							tsdk = vim.fn.getcwd() .. "node_modules/typescript/lib",
+						},
+					},
 					settings = {},
 				})
 			end,
