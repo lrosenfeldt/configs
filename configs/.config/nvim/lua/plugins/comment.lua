cb545bdf16af7533d12677951ab0dc7e9d5523bd
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-04-24 08:41:08 +0200

    feat: use builtin comment function

--- configs/.config/nvim/lua/plugins/comment.lua
+++
@@ -1,6 +0,0 @@
-return {
-	-- "gc" to comment visual regions/lines
-	"echasnovski/mini.comment",
-	event = "VeryLazy",
-	opts = {},
-}
--- configs/.config/nvim/lua/set.lua
+++ configs/.config/nvim/lua/set.lua
@@ -25,7 +25,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 15
 
 vim.opt.colorcolumn = "100"
 
