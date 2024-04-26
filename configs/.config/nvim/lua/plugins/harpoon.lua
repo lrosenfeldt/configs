6c1bb294ff9c83b184d19f4fd032ef632a28eda2
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-04-26 19:53:45 +0200

    refactor: use harpoon v2

--- configs/.config/nvim/lua/plugins/harpoon.lua
+++ configs/.config/nvim/lua/plugins/harpoon.lua
@@ -1,55 +1,46 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	keys = {
-		{
-			"<leader>hm",
-			function()
-				require("harpoon.mark").add_file()
-			end,
-			desc = "(Harpoon) [m]ark file",
-		},
-		{
-			"<leader>ht",
-			function()
-				require("harpoon.ui").toggle_quick_menu()
-			end,
-			desc = "(Harpoon) [t]oggle quick menu",
-		},
-		{
-			"<C-h>",
-			function()
-				require("harpoon.ui").nav_file(1)
-			end,
-			desc = "(Harpoon) jump to first file",
-		},
-		{
-			"<C-j>",
-			function()
-				require("harpoon.ui").nav_file(2)
-			end,
-			desc = "(Harpoon) jump to second file",
-		},
-		{
-			"<C-k>",
-			function()
-				require("harpoon.ui").nav_file(3)
-			end,
-			desc = "(Harpoon) jump to third file",
-		},
-		{
-			"<C-l>",
-			function()
-				require("harpoon.ui").nav_file(4)
-			end,
-			desc = "(Harpoon) jump to fourth file",
-		},
+		"<leader>hm",
+		"<leader>ht",
+		"<C-h>",
+		"<C-j>",
+		"<C-k>",
+		"<C-l>",
 	},
-	opts = {
-		menu = {
-			width = 80,
-		},
-	},
+  opts = {
+    settings = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+    },
+  },
+	config = function(_, opts)
+		local harpoon = require("harpoon")
+
+		harpoon:setup(opts)
+
+		vim.keymap.set("n", "<leader>hm", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>ht", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-j>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-k>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-l>", function()
+			harpoon:list():select(4)
+		end)
+	end,
 }
