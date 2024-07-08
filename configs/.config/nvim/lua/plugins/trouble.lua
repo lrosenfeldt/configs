872bbaacd3ab795b7cbcbb01ac8f2141d4ab4d88
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-07-08 09:34:21 +0200

    update lazy deps and use new api for trouble

--- configs/.config/nvim/lua/plugins/trouble.lua
+++ configs/.config/nvim/lua/plugins/trouble.lua
@@ -4,31 +4,49 @@ return {
 	keys = {
 		{
 			"<leader>xq",
-			"<cmd>TroubleToggle quickfix<cr>",
+			"<cmd>Trouble qflist toggle<cr>",
 			desc = "Trouble: toggle [q]uickfix",
 			silent = true,
 			noremap = true,
 		},
 		{
 			"<leader>xw",
-			"<cmd>TroubleToggle workspace_diagnostics<cr>",
+			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Trouble: toggle [w]orkspace diagnostics",
 			silent = true,
 			noremap = true,
 		},
 	},
 	opts = {
-		icons = false,
-		fold_open = "v", -- icon used for open folds
-		fold_closed = ">", -- icon used for closed folds
-		indent_lines = false, -- add an indent guide below the fold icons
-		signs = {
-			-- icons / text used for a diagnostic
-			error = "E",
-			warning = "W",
-			hint = "H",
-			information = "I",
+		icons = {
+			folder_closed = ">", -- icon used for closed folds
+			folder_open = "v", -- icon used for open folds
+			Array = "",
+			Boolean = "",
+			Class = "",
+			Constant = "",
+			Constructor = "",
+			Enum = "",
+			EnumMember = "",
+			Event = "",
+			Field = "",
+			File = "",
+			Function = "",
+			Interface = "",
+			Key = "",
+			Method = "",
+			Module = "",
+			Namespace = "",
+			Null = "",
+			Number = "",
+			Object = "",
+			Operator = "",
+			Package = "",
+			Property = "",
+			String = "",
+			Struct = "",
+			TypeParameter = "",
+			Variable = "",
 		},
-		use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
 	},
 }
